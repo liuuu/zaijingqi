e5bcbaa914e88c519f1ee908537e5feb44e5711a
liuuu <lyloft@gmail.com>
2026-05-13 12:43:23 +0800

    feat: add banner

--- miniprogram/pages/activities/index.js
+++ miniprogram/pages/activities/index.js
@@ -2,5 +2,18 @@ Page({
   data: {
     title: "Activities",
     description: "This is a stub page for the activities tab.",
+    imageProps: {
+      mode: "aspectFit",
+    },
+    swiperList: [
+      {
+        value: "/images/zaijingqi.JPG",
+        ariaLabel: "zaijingqi",
+      },
+      {
+        value: "/images/zaijingqi1.JPG",
+        ariaLabel: "zaijingqi1",
+      },
+    ],
   },
 });
